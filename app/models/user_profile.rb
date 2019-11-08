3e2d19603575a0a0c8a7eef1d228d6c16512320e
Ramesh-Veeriaperumal <ramesh.veeriaperumal@freshworks.com>
2019-11-08 15:45:23 +0530

    Controller and view codes

---
+++ app/models/user_profile.rb
@@ -0,0 +1,5 @@
+class UserProfile < ApplicationRecord
+  has_secure_password
+
+  validates :email, presence: true, uniqueness: true
+end
---
+++ test/system/user_profiles_test.rb
@@ -0,0 +1,49 @@
+require "application_system_test_case"
+
+class UserProfilesTest < ApplicationSystemTestCase
+  setup do
+    @user_profile = user_profiles(:one)
+  end
+
+  test "visiting the index" do
+    visit user_profiles_url
+    assert_selector "h1", text: "User Profiles"
+  end
+
+  test "creating a User profile" do
+    visit user_profiles_url
+    click_on "New User Profile"
+
+    fill_in "Email", with: @user_profile.email
+    fill_in "Name", with: @user_profile.name
+    fill_in "Password", with: 'secret'
+    fill_in "Password confirmation", with: 'secret'
+    click_on "Create User profile"
+
+    assert_text "User profile was successfully created"
+    click_on "Back"
+  end
+
+  test "updating a User profile" do
+    visit user_profiles_url
+    click_on "Edit", match: :first
+
+    fill_in "Email", with: @user_profile.email
+    fill_in "Name", with: @user_profile.name
+    fill_in "Password", with: 'secret'
+    fill_in "Password confirmation", with: 'secret'
+    click_on "Update User profile"
+
+    assert_text "User profile was successfully updated"
+    click_on "Back"
+  end
+
+  test "destroying a User profile" do
+    visit user_profiles_url
+    page.accept_confirm do
+      click_on "Destroy", match: :first
+    end
+
+    assert_text "User profile was successfully destroyed"
+  end
+end
